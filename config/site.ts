e3e94b5830471d9a5458a6e6387930b241073c87
EDRES AREF ALI MOQBEL <orbit.ops.digital@gmail.com>
2026-07-18 21:32:43 +0400

    Complete homepage branding and configuration

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export const siteConfig = {
+  name: "ORBIT | مَدار",
+  description: "...",
+
+  url: "https://your-domain.com",
+
+  email: "orbit.ops.digital@gmail.com",
+
+  phone: "+967735509366",
+
+  social: {
+    github: "https://github.com/madarorbit/madar-store.git",
+    linkedin: "",
+    x: "https://x.com/ORBITewrt",
+    whatsapp: "https://t.co/15TSWmv34p",
+  },
+};
